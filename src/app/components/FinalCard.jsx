9c06c853d3901a6eeb98a0deeeea03423eae455d
Anuj Chaudhary <alwaysmile9813@gmail.com>
2025-05-10 15:04:38 +0530

    `updated`

--- src/app/components/FinalCard.jsx
+++ src/app/components/FinalCard.jsx
@@ -112,7 +112,7 @@ export default function FinalCard() {
                                             </motion.div>
                                         </div>
 
-                                        <h3 className="text-3xl md:text-4xl font-bold text-pink-600 mb-2">From your biggest fan,</h3>
+                                        <h3 className="text-3xl md:text-4xl font-bold text-pink-600 mb-2">With all my love,</h3>
 
                                         <h3 className="text-4xl md:text-5xl font-bold text-purple-600">Anuj</h3>
                                     </motion.div>
